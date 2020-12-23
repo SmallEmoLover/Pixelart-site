6f476a905c1a43692cca5703682ab1d875317df1
SmallEmoLover <missingtext@yandex.ru>
2020-12-23 17:52:52 +0300

    Инициация. Скрипт на параллакс. Анимированые облака.

---
+++ parallax.js
@@ -0,0 +1,11 @@
+const abc = document.getElementById("clouds");
+
+document.addEventListener("mousemove", function (e) { moveMe(e); });
+
+function moveMe(e) {
+    let offsetX = (e.clientX / window.innerWidth * 50);
+	let offsetY = (e.clientY / window.innerHeight * 20);
+
+	//Меняем положение фона
+	abc.setAttribute("style", "background-position: " + offsetX + "px " + offsetY + "px;");
+};
